339df9072a9870fb50e425b177429a4a2ceabb68
wangy <wangy@transip.nl>
2023-07-31 16:23:40 +0200

    import bootstrap and add a carView

--- pingpong-spa/src/components/GameComponent.vue
+++ pingpong-spa/src/components/GameComponent.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="card" style="width: 18rem">
+  <div class="card" style="width: 20rem">
     <img src="..." class="card-img-top" alt="..." />
     <div class="card-body">
       <p class="card-text">card view</p>
+      <button type="button" class="btn btn-primary">hello</button>
     </div>
   </div>
 </template>
